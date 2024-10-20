b218cb9fab497ff6732899c2a0e244681693dd18
ewaldmikolaj <mikolajewald@gmail.com>
2024-10-20 18:15:18 +0200

    login page css changed

--- ExpensesTracker/tailwind.config.js
+++ ExpensesTracker/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './Pages/**/*.cshtml',
+    './**/Pages/**/*.cshtml',
     './Views/**/*.cshtml'
   ],
   theme: {
@@ -9,4 +9,3 @@ module.exports = {
   },
   plugins: [],
 };
-
