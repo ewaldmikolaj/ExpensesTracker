454496535e89213469d45416fd29833c3fbbb152
ewaldmikolaj <mikolajewald@gmail.com>
2024-10-20 14:07:51 +0200

    Initial commit

---
+++ ExpensesTracker/Areas/Identity/Data/ExpensesTrackerIdentityDbContext.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpensesTracker.Areas.Identity.Data;
+
+public class ExpensesTrackerIdentityDbContext : IdentityDbContext<IdentityUser>
+{
+    public ExpensesTrackerIdentityDbContext(DbContextOptions<ExpensesTrackerIdentityDbContext> options)
+        : base(options)
+    {
+    }
+    
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.HasDefaultSchema("identity");
+    }
+}
