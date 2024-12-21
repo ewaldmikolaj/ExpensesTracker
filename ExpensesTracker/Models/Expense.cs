fceaa6b9e83c5917867e6c7deeeabaf60a10b31d
ewaldmikolaj <mikolajewald@gmail.com>
2024-12-21 16:22:50 +0100

    Validation improves

--- ExpensesTracker/Models/Expense.cs
+++ ExpensesTracker/Models/Expense.cs
@@ -1,17 +1,25 @@
 using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace ExpensesTracker.Models;
 
 public class Expense
 {
 	public int Id { get; set; }
+	[Required(ErrorMessage = "{0} jest wymagany.")]
+	[StringLength(100, ErrorMessage = "{0} może mieć maksymalnie 100 znaków.")]
 	[Display(Name = "Tytuł")]
 	public string Title { get; set; }
+	[Required(ErrorMessage = "{0} jest wymagana.")]
+	[DataType(DataType.DateTime, ErrorMessage = "Wartość musi być poprawną datą.")]
+	[DisplayFormat(DataFormatString = "{0:dd/MM/yyyy HH:mm}")]
 	[Display(Name = "Data")]
-	[DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
-	public DateTime Date { get; set; }
+	public DateTime? Date { get; set; }
+	[Required(ErrorMessage = "{0} jest wymagana.")]
+	[DataType(DataType.Currency, ErrorMessage = "Wartość {0} nie jest prawidłową kwotą.")]
+	[Range(0, double.MaxValue, ErrorMessage = "{0} musi być większa od zera.")]
 	[Display(Name = "Kwota")]
-	public decimal Amount { get; set; }
+	public decimal? Amount { get; set; }
 	[Display(Name = "Płatnik")]
 	public string? PayerId { get; set; }
 	public virtual ApplicationUser? Payer { get; set; }
--- ExpensesTracker/Models/List.cs
+++ ExpensesTracker/Models/List.cs
@@ -1,10 +1,13 @@
 using System.ComponentModel.DataAnnotations;
+using Microsoft.VisualBasic;
 
 namespace ExpensesTracker.Models;
 
 public class List
 {
 	public int Id { get; set; }
+	[Required(ErrorMessage = "{0} jest wymagana.")]
+	[StringLength(100, ErrorMessage = "{0} może mieć maksymalnie 100 znaków.")]
 	[Display(Name = "Nazwa")]
 	public string Name { get; set; }
 	public string? OwnerId {get; set;}
