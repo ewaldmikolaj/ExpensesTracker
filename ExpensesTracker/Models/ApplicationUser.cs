2cbdf5b3dfefe44bff8416a4a304fbc579d3d0fc
ewaldmikolaj <mikolajewald@gmail.com>
2024-12-22 23:19:48 +0100

    limitations in application user added

--- ExpensesTracker/Models/ApplicationUser.cs
+++ ExpensesTracker/Models/ApplicationUser.cs
@@ -1,9 +1,14 @@
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
 
 namespace ExpensesTracker.Models;
 
 public class ApplicationUser : IdentityUser
 {
+	[StringLength(100, ErrorMessage = "{0} może mieć maksymalnie 100 znaków.")]
+	[Display(Name = "Imię")]
 	public string Name {get; set;}
+	[StringLength(100, ErrorMessage = "{0} może mieć maksymalnie 100 znaków.")]
+	[Display(Name = "Nazwisko")]
 	public string Surname { get; set; }
 }
